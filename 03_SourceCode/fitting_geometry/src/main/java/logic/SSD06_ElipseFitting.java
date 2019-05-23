83d8ed52378d7ef7dc8773f3b220203257b39fcb
dangnam8298 <44374063+dangnam8298@users.noreply.github.com>
2019-05-24 00:29:28 +0700

    Create SSD06_ElipseFitting.java

---
+++ 03_SourceCode/fitting_geometry/src/main/java/logic/SSD06_ElipseFitting.java
@@ -0,0 +1,17 @@
+package logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Point2D;
+
+public class SSD06_ElipseFitting {
+
+	public List<Double> fitElipseByLMS(List<Point2D> listPoint){
+		List<Double> listOutput = new ArrayList<Double>();
+		
+		
+		
+		return listOutput;
+	}
+}
